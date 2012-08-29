b444f52c0861b27b08d62702ebba5964602c5345
Eric Gjertsen <ericgj72@gmail.com>
2012-08-29 19:31:18 -0400

    + top-level interface Wineskins.transfer + VERSION = 0.0.1

--- lib/wineskins.rb
+++ lib/wineskins.rb
@@ -1,12 +1,19 @@
 require 'sequel'
 require File.expand_path('sequel_ext/adapters/shared/access', File.dirname(__FILE__))
 
+require File.expand_path('wineskins/version', File.dirname(__FILE__))
 require File.expand_path('wineskins/utils', File.dirname(__FILE__))
 require File.expand_path('wineskins/schema_methods', File.dirname(__FILE__))
 require File.expand_path('wineskins/record_methods', File.dirname(__FILE__))
 
 module Wineskins
 
+  def self.transfer(source, dest, &block)
+    dest.transaction do
+      Transfer.new(source, dest).instance_eval(&block)
+    end
+  end
+    
   class Transfer
     include SchemaMethods
     include RecordMethods
---
+++ lib/wineskins/version.rb
@@ -0,0 +1,3 @@
+﻿module Wineskins
+  VERSION = '0.0.1'
+end
